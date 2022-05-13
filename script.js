4a9d35e38f5094663691339cd40ea6a372ff6ce5
jleannaw <95251563+jleannaw@users.noreply.github.com>
2022-05-13 12:20:20 -0400

    round tres!!!

---
+++ script.js
@@ -0,0 +1,10 @@
+const textList = ["gardening.", "cats.", "design.", "food."];
+
+const cycle = document.querySelector("#cycle");
+let i = 0;
+const cycleText = () => {
+  cycle.innerHTML = textList[i];
+  i = ++i % textList.length;
+};
+cycleText();
+setInterval(cycleText, 1500);
